0d104a1f6b6b62c9767888d29e8ecf76505bb0bb
mybene <benoite251@gmial.com>
2019-09-23 14:55:46 +0200

    test sightingAnimals

--- src/main/java/models/SightingAnimals.java
+++ src/main/java/models/SightingAnimals.java
@@ -1,4 +1,36 @@
 package models;
 
 public class SightingAnimals {
+   public int id;
+   public String location;
+   public String rangeName;
+
+    public SightingAnimals(String location, String rangeName) {
+        this.location = location;
+        this.rangeName = rangeName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getRangeName() {
+        return rangeName;
+    }
+
+    public void setRangeName(String rangeName) {
+        this.rangeName = rangeName;
+    }
 }
--- src/test/java/SightingAnimalsTest.java
+++ src/test/java/SightingAnimalsTest.java
@@ -1,2 +1,40 @@
-public class SightingAnimalsTest {
+import models.SightingAnimals;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class SightingAnimalsTest{
+
+    SightingAnimals testSightingAnimals;
+
+    @Before
+    public void iniatateUp() {
+        SightingAnimals testSightingAnimals = new SightingAnimals();
+    }
+
+    @Test
+    public void testSightingAnimals_instantiatesCorrectly_true() {
+        assertTrue(testSightingAnimals instanceof SightingAnimals);
+    }
+
+    @Test
+    public void getId_instantiatesCorrectly_true() {
+        assertEquals(1, true, testSightingAnimals.getId());
+    }
+
+    @Test
+    public void location_instantiatesCorrectly_true() {
+
+        assertEquals("Zone A", testSightingAnimals.getLocation());
+    }
+
+    @Test
+    public void rangerName_instantiatesCorrectly_true() {
+        assertEquals("David", testSightingAnimals.getRangeName());
+
+    }
 }
